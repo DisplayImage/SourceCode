9283372b4e06036d342abc263a2ffcf541546cea
thuantran <thuantq.uit@gmail.com>
2016-06-05 14:55:09 +0700

    update

--- ProgramAnalysis/Controllers/HomeController.cs
+++ ProgramAnalysis/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ExifMetadata.Exif;
+using ProgramAnalysis.Helpers;
+using ProgramAnalysis.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +13,25 @@ namespace ProgramAnalysis.Controllers
     {
         public ActionResult Index()
         {
+            return View();
+        }
+        public ActionResult XuLy(InfoImageMV model)
+        {
+            var infoImage = new ExifTagCollection(model.ImagePath);
+            foreach (ExifTag elm in infoImage)
+            {
+                model.ResultImage.Add(elm);
+            }
+            MatLabConfig mark = new MatLabConfig();
+            mark.MatlabObj.Execute("clc; clear");
+            mark.MatlabObj.Execute("cd " + mark.matlabFuncPath);
+            ImageInfoMark item = new ImageInfoMark();
+            //model.ImagePath = mark.matlabDataPath + "\\THMILKImages\\10000315_SM000736_C000117994_1456718517808.jpg";
+            item = mark.ImageReal(model.ImagePath);
+            model.ListItem.Add(item);
+            item = mark.ItemExistImage(model.ImagePath);
+            model.ListItem.Add(item);
+            mark.MatlabObj.Quit();
             return View();
         }
     }
--- ProgramAnalysis/ProgramAnalysis/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProgramAnalysis.WebApiApplication" Language="C#" %>
